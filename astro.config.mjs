8cdf0c03f0d7b13176367bb2d9ed666688c4aedc
Kolos Káposzta <kap.kolos@gmail.com>
2023-01-30 21:56:12 +0100

    tailwind init

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,30 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 
 // https://astro.build/config
-import partytown from "@astrojs/partytown";
+import partytown from '@astrojs/partytown';
+
+// https://astro.build/config
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://valamikolos.hu',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap(), partytown()]
+  integrations: [
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    sitemap(),
+    partytown(),
+    tailwind({
+      config: { path: './tailwind.config.cjs' },
+    }),
+  ],
 });
